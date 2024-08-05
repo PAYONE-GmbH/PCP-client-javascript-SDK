45e0e513ba56c1cf86fb6d2c9d0ba66afb494121
Dirk Ehrhardt <d.ehrhardt@nanogiants.de>
2024-08-05 15:37:39 +0200

    refactor(apple pay session): update demo application data to use base64 encoding

--- applepay-demo/src/main.ts
+++ applepay-demo/src/main.ts
@@ -1,5 +1,6 @@
 import {
   ApplePayButton,
+  encodeToBase64,
   PCPApplePaySession,
   PCPApplePaySessionConfig,
 } from 'pcp-client-javascript-sdk';
@@ -37,15 +38,7 @@ const init = async () => {
     validateMerchantURL: import.meta.env.VITE_APPLE_PAY_VALIDATE_MERCHANT_URL,
     processPaymentURL: import.meta.env.VITE_APPLE_PAY_PROCESS_PAYMENT_URL,
     // This data is completely custom and needs to be sent to your server for merchant validation and must be used as a base64 encoded string here for the apple pay server
-    applicationData: btoa(
-      unescape(
-        encodeURIComponent(
-          JSON.stringify({
-            foo: 'bar',
-          }),
-        ),
-      ),
-    ),
+    applicationData: encodeToBase64(JSON.stringify({ foo: 'bar' })),
     paymentMethodSelectedCallback: async (paymentMethod) => {
       console.log('paymentMethodSelectedCallback', paymentMethod);
       return {
